b06b2737d91512bdf5471f53a45bc46ef7672cf4
Aruanzin <Aruanbretas@usp.br>
2025-04-14 14:10:30 -0300

    automatos que iremos ter que construir

--- lexico.c
+++ lexico.c
@@ -20,8 +20,24 @@ tabelaReservados[] = {
 operadores[] = {
     ";", ":", "+", "-", "*", "/", "(", ")", "=", ",", ">", "<", "."
 };
+//adiciona os tokens que coletamos a tabela com sua tipagem e tudo mais
+int addToken(){
 
+}
+//identifica operador
+void automatoOperador(){
+    
+}
+//identica identificador ou palavras reservadas
+void automatoIdentificador(){
+    
+}
+//identifica boa formação de numeros inteiros ou reais
+void automatoNumero(){
 
+}
+
+//fazer aqui o começo da analise para saber qual automato acionar
 void lexico(const char* linha, int num_linha){
     int pointer = 0;
 
